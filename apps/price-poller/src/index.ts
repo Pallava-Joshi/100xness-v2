80ab25ce197c80421d0c4482545a799fe85372a1
Pallava-Joshi <joshipallava@gmail.com>
2025-09-14 23:46:18 +0530

    db init

--- apps/price-poller/src/index.ts
+++ apps/price-poller/src/index.ts
@@ -1,4 +1,21 @@
 import WebSocket, { WebSocketServer } from "ws";
 import dotenv from "dotenv";
+
 dotenv.config();
-const ws = new WebSocket(process.env.WEBSOCKET_URL);
+const url = process.env.WEBSOCKET_URL || "";
+
+const ws = new WebSocket(url);
+ws.on("error", console.error);
+
+ws.on("open", function open() {
+  const payload = {
+    method: "SUBSCRIBE",
+    params: ["depth.SOL_USDC"],
+  };
+
+  ws.send(JSON.stringify(payload));
+});
+
+ws.on("message", function message(data) {
+  console.log("received: %s", data);
+});
